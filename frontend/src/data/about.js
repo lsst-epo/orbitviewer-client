30267fe2ac53f83a1ca90e845d1d7016278e34c9
Martí Fenosa <marti.fenosa@gmail.com>
2022-08-11 10:28:53 +0200

    About

--- frontend/src/data/about.js
+++ frontend/src/data/about.js
@@ -1,31 +1,30 @@
+const getQuery = require('../../utils/getQuery');
+
+async function getAbout() {
+  const query = `
+  {
+    entries(section: "about") {
+			...on about_about_Entry {
+				siteTitle
+				siteDescription
+				pageContent
+			}
+		}
+  }`;
+
+  const data = await getQuery(query);
+
+  const d = data.data.entries[0];
+  
+  const formatted = {
+    title: d.siteTitle,
+		description: d.siteDescription,
+		content: d.pageContent
+  }
+
+  return formatted;
+}
 
 
-// const fetch = require('node-fetch');
-
-// const token = "mklNxBQSfDf69uUoOWvtyVK4P6pe9h7r";
-
-// async function getContent() {
-
-//   const recordsPerQuery = 100;
-
-//   // number of records to skip (start at 0)
-//   let recordsToSkip = 0;
-
-//   // do we make a query ?
-//   let makeNewQuery = true;
-
-//   // Blogposts array
-//   let blogposts = [];
-
-// }
-
-// query MyQuery {
-//   entries(section: "about") {
-//     ...on about_about_Entry {
-//       siteTitle
-//       siteDescription
-//       pageContent
-//     }
-//   }
-// }
-// Authorization: Bearer mklNxBQSfDf69uUoOWvtyVK4P6pe9h7r
+// export for 11ty
+module.exports = getAbout;
