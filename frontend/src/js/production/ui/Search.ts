7343ca31a2338b4d2751e9a78fc1dde430b813a6
Martí Fenosa <marti.fenosa@gmail.com>
2022-08-16 18:31:43 +0200

    WIP search

--- frontend/src/js/production/ui/Search.ts
+++ frontend/src/js/production/ui/Search.ts
@@ -1,12 +1,39 @@
 
+enum STATE {
+	IDLE,
+	ACTIVE,
+	FOCUS
+}
 
 export class Search {
 	dom: HTMLElement;
+	state: STATE = 0;
 	constructor(dom: HTMLElement = null){
 
 		if(!!!dom) return;
 
-		this.dom = dom;
+		this.dom = dom.querySelector('.search');
+
+		this.addEventListeners();
+
+	}
+
+	addEventListeners(){
+
+		const input = this.dom.querySelector('.search-text-input');
+
+		const lenseButton = this.dom.querySelector('.search-lense');
+		lenseButton.addEventListener('click', () => {
+			if(this.state === 0){
+				this.state = 1;
+			}
+			this.dom.setAttribute('data-state', this.state.toString());
+		})
+
+		const closeButton = this.dom.querySelector('.search-reset');
+		closeButton.addEventListener('click', () => {
+			input.value = ''
+		})
 
 	}
 }
