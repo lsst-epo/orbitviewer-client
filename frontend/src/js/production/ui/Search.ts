a0fb04c84b5e3366079a04bd34655eea83f8b1b5
Martí Fenosa <marti.fenosa@gmail.com>
2022-08-18 09:54:03 +0200

    Search close with ESCAPE

--- frontend/src/js/production/ui/Search.ts
+++ frontend/src/js/production/ui/Search.ts
@@ -20,7 +20,7 @@ export class Search {
 
 	addEventListeners(){
 
-		const input = this.dom.querySelector('.search-text-input input');
+		const input = this.dom.querySelector('.search-text-input input') as HTMLInputElement;
 		input.addEventListener('input', () => {						
 			if(input.value.length > 0) this.updateState(2);
 			else this.updateState(1);
@@ -39,6 +39,18 @@ export class Search {
 
 		const cancelButton = this.dom.querySelector('.search-cancel');
 		cancelButton.addEventListener('click', () => {			
+			input.value = '';
+			this.updateState(0);
+		})
+
+		document.addEventListener('keydown', (e) => {			
+
+			if(e.key != 'Escape') return;
+
+			if(this.state === 0) return;
+
+			e.preventDefault();
+
 			input.value = '';
 			this.updateState(0);
 		})
