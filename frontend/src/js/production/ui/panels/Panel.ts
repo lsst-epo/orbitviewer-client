d546711c8d7458e6d2cfdd1fa4766a48f6c53973
Martí Fenosa <marti.fenosa@gmail.com>
2022-08-22 16:54:50 +0200

    Index tweaking

--- frontend/src/js/production/ui/panels/Panel.ts
+++ frontend/src/js/production/ui/panels/Panel.ts
@@ -22,8 +22,16 @@ export class Panel {
 	togglePanel(){		
 		this.active = !this.active;
 		this.dom.classList.toggle('active');
-	}
 
+		if(this.active){
+			this.dom.classList.add('to-front');
+		} else {
+			setTimeout(() => {
+				this.dom.classList.remove('to-front');
+			}, 500);
+		}
+	}
+ 
 	addEventListeners(){
 		// Panel Buttons
 		const buttons = document.querySelectorAll(`[data-panel-button="${this.id}"]`);
