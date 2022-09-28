4a0fa90844b418268b0a890ef6e50ab8424ca1e7
Martí Fenosa <marti.fenosa@gmail.com>
2022-09-28 13:14:25 +0200

    Loader update

--- src/js/production/ui/loader.ts
+++ src/js/production/ui/loader.ts
@@ -7,11 +7,39 @@ const loader = document.querySelector('.loader');
 const svg = loader.querySelector('svg');
 const elements = svg.querySelectorAll('path, circle');
 
-const tl = gsap.timeline({ paused: true, repeat: -1 })
+const tl2 = gsap.timeline({ paused: true, repeat: -1 })
+const tl1 = gsap.timeline({ paused: true, repeat: -1 })
+
+tl1
+	.addLabel('start')
+	.to(svg, {
+		transformOrigin: 'center',
+		rotate: '+=360',
+		ease: 'linear',
+		duration: 50
+	})
+
+tl2
+	.set(elements, { autoAlpha: 0 })
+	.addLabel('start')
+	.to(elements, {
+		stagger: 0.04,
+		autoAlpha: 1,
+		duration: 0.6,
+		ease: 'power3.out'
+	})
+	.to(elements, {
+		delay: 0.5,
+		autoAlpha: 0,
+		duration: 1,
+		ease: 'power2.out'
+	})
 
 const resetTl = () => {
-	tl.pause();
-	tl.progress(0);
+	tl1.pause();
+	tl1.progress(0);
+	tl2.pause();
+	tl2.progress(0);
 
 	gsap.set(svg, {
 		transformOrigin: 'center',
@@ -26,7 +54,8 @@ export const showLoader = () => {
 	active = true;
 	document.body.classList.add('loader-active');
 
-	tl.play();
+	tl1.play();
+	tl2.play();
 
 	gsap.to(loader, {
 		autoAlpha: 1,
