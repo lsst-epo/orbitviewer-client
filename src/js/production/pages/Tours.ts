f255c19d8e29b7319fe9989b2e2394f358defd9e
Martí Fenosa <marti.fenosa@gmail.com>
2022-09-30 16:05:59 +0200

    Chevron anim v1

--- src/js/production/pages/Tours.ts
+++ src/js/production/pages/Tours.ts
@@ -1,6 +1,53 @@
+import gsap from "gsap/all";
 import { Page } from "./Page";
 
 
 export class Tours extends Page {
+	chevs:NodeListOf<HTMLElement>;
+	playing: boolean = false;
+
+	onLoaded(){
+		this.chevs = this.dom.querySelectorAll('.tour-pagination svg');
+	}
+
+	playChev(chev, direction){
+		if(this.playing) return;
+		this.playing = true;
+
+		gsap.to(chev, {
+			duration: 0.2,
+			x: direction === 'left' ? '-=10px' : '+=10px',
+			scaleY: 0.7,
+			transformOrigin: 'center',
+			ease: 'power2.out',
+			onComplete: () => {
+				gsap.to(chev, {
+					duration: 0.6,
+					x: 0,
+					scaleY: 1,
+					transformOrigin: 'center',
+					ease: 'power2.in',
+					onComplete: () => {
+						this.playing = false;
+					}
+				});
+			}
+		})
+
+	}
+
+	addEventListeners(): void {
+		super.addEventListeners();
+		console.log(this.chevs);
+		
+		this.chevs[0].addEventListener('mouseenter', () => {
+			this.playChev(this.chevs[0], 'left')
+		})
+		this.chevs[1].addEventListener('mouseenter', () => {
+			this.playChev(this.chevs[1], 'right')
+		})
+	}
+
+
 
 }
