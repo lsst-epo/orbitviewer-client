edcedae7089f709567f818f3c31176d0cade9115
Martí Fenosa <marti.fenosa@gmail.com>
2022-09-30 14:23:06 +0200

    Gsap warnings fix

--- src/js/production/pagination/animations/OrbitControls.ts
+++ src/js/production/pagination/animations/OrbitControls.ts
@@ -2,29 +2,46 @@ import { LOCATION } from "../History";
 
 export const OrbitControlsOut = (tl:GSAPTimeline, label:string) => {
 	const dom = LOCATION.previous.class.dom;
-	const slide = dom.querySelectorAll('[data-animation-type="slide"]');
 
 	const left = dom.querySelectorAll('[data-animation-type="control-left"]');
-	const right = dom.querySelectorAll('[data-animation-type="control-right"]');
-	const bottom = dom.querySelectorAll('[data-animation-type="control-bottom"]');
+	if(left.length > 0){
+		tl.to(left, {
+			autoAlpha: 0,
+			x: '-10px',
+			duration: 0.3
+		}, label)
+	}
 
-	tl.to(slide, {
-		autoAlpha: 0,
-		y: '10px',
-		duration: 0.3
-	}, label)
+	const right = dom.querySelectorAll('[data-animation-type="control-right"]');
+	if(right.length > 0){
+		tl.to(right, {
+			autoAlpha: 0,
+			x: '10px',
+			duration: 0.3
+		}, label)
+	}
+
+	const bottom = dom.querySelectorAll('[data-animation-type="control-bottom"]');
+	if(bottom.length > 0){
+		tl.to(bottom, {
+			autoAlpha: 0,
+			y: '10px',
+			duration: 0.3
+		}, label)
+	}
+
 
 }
 export const OrbitControlsIn = (tl:GSAPTimeline, label:string) => {
 	const dom = LOCATION.current.class.dom;
 	const control = dom.querySelectorAll('[data-animation-type^="control-"]');
 
-	const duration = 0.4;
+	if(control.length === 0) return;
 
 	tl.to(control, {
 		autoAlpha: 1,
 		y: 0,
 		x: 0,
-		duration
+		duration: 0.4
 	}, label)
 }
--- src/js/production/pagination/animations/Slide.ts
+++ src/js/production/pagination/animations/Slide.ts
@@ -3,6 +3,8 @@ import { LOCATION } from "../History";
 export const SlideOut = (tl:GSAPTimeline, label:string) => {
 	const dom = LOCATION.previous.class.dom;
 	const slide = dom.querySelectorAll('[data-animation-type="slide"]');
+	
+	if(slide.length === 0) return;
 
 	tl.to(slide, {
 		autoAlpha: 0,
@@ -13,6 +15,8 @@ export const SlideOut = (tl:GSAPTimeline, label:string) => {
 export const SlideIn = (tl:GSAPTimeline, label:string) => {
 	const dom = LOCATION.current.class.dom;
 	const slide = dom.querySelectorAll('[data-animation-type="slide"]');
+
+	if(slide.length === 0) return;
 
 	tl.to(slide, {
 		autoAlpha: 1,
