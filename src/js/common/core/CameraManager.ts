03c2561654dbfe01cf030667ca497c0f28411e29
Edu Prats <eduprats@gmail.com>
2022-10-05 15:39:18 +0200

    New Camera System

--- src/js/common/core/CameraManager.ts
+++ src/js/common/core/CameraManager.ts
@@ -21,19 +21,34 @@ export const CameraSettings = {
         zoomSpeed: 1.2
     },
     traveling: {
-        easing: .016
+        easing: .06
     }
 }
 
 const TARGET = {
-    obj: new Object3D(),
-    prevPos: new Vector3(),
-    prevRot: new Quaternion()
+    obj: new Object3D()
 }
 
 const origin = new Vector3();
 const tmp = new Vector3();
-const tmp2 = new Vector3();
+
+export type ShpericalCoords = {
+    radius:number;
+    angle:number;
+    elevation:number;
+}
+
+const sphericalCoords:ShpericalCoords = {
+    radius: 0,
+    angle: 0,
+    elevation: 0
+}
+
+const prevSC:ShpericalCoords = {
+    radius: 0,
+    angle: 0,
+    elevation: 0
+}
 
 /**
  * Camera Controller Class
@@ -46,6 +61,7 @@ class CameraController {
     cam:PerspectiveCamera;
     private orbit:boolean = false;
     private currentTarget:InteractiveObject = null;
+    private unlocking:boolean = false;
 
     get active():boolean {
         return this.initialized;
@@ -65,60 +81,135 @@ class CameraController {
         this.controls.enableDamping = true;
         this.controls.dampingFactor = s.damping;
         this.controls.zoomSpeed = s.zoomSpeed;
+
+        const sc = this.getSC(DEFAULT_CAM_POS);
+        this.copySC(sc, sphericalCoords);
+        this.copySC(sc, prevSC);
+    }
+
+    private getSC(p:Vector3):ShpericalCoords {
+        const R = Math.sqrt(p.x*p.x+p.z*p.z);
+        const angle = Math.atan2(p.z, p.x);
+
+        return {
+            angle: angle,
+            radius: R,
+            elevation: p.y
+        }
+    }
+
+    private refreshSC() {
+        const sc = this.getSC(this.cam.position);
+        this.copySC(sc, sphericalCoords);
+    }
+
+    private copySC(src:ShpericalCoords, dst:ShpericalCoords) {
+        dst.radius = src.radius;
+        dst.angle = src.angle;
+        dst.elevation = src.elevation;
+    }
+
+    private updateTarget() {
+        const target = this.currentTarget;
+        const R = sphericalCoords.radius;
+        const x = R * Math.cos(sphericalCoords.angle);
+        const y = sphericalCoords.elevation;
+        const z = R * Math.sin(sphericalCoords.angle);
+
+        TARGET.obj.position.set(x,y,z);
+        if(target) {
+            TARGET.obj.translateX(target.lockedOffset.x);
+            TARGET.obj.translateY(target.lockedOffset.y);
+            TARGET.obj.translateZ(target.lockedOffset.z);
+        }
+    }
+
+    private getTargetSC(target:Object3D):ShpericalCoords {
+        const sc = this.getSC(target.position);
+        const dA = Math.abs(sphericalCoords.angle - sc.angle);
+
+        if(dA > Math.PI) {
+            console.log('correct angle');
+            
+            if(sc.angle > Math.PI) sc.angle -= 2*Math.PI;
+            else sc.angle += 2*Math.PI;
+        }
+
+        return sc;
     }
 
     goToTarget(target:InteractiveObject, orbitAround:boolean=false) {
         if(!this.initialized) return console.warn("CameraController not initialized! Please run CameraManager.init() first.");
         this.killTweens();
         this.mode = CameraMode.LOCKED;
-        target.target.getWorldPosition(TARGET.obj.position);
-        tmp.copy(TARGET.obj.position);
-        tmp2.copy(origin).sub(tmp).normalize().multiplyScalar(target.lockedDistance);
-        TARGET.obj.position.sub(tmp2)
-        TARGET.obj.translateX(target.lockedOffset.x);
-        TARGET.obj.translateY(target.lockedOffset.y);
-        TARGET.obj.translateZ(target.lockedOffset.z);
-        TARGET.obj.lookAt(origin);
-        TARGET.prevPos.copy(this.cam.position);
-        TARGET.prevRot.copy(this.cam.quaternion);
-        this.orbit = orbitAround;
         this.currentTarget = target;
+        this.refreshSC();
+        this.copySC(sphericalCoords, prevSC);
+        const sc = this.getTargetSC(target.target);
+
+        this.unlocking = false;
+
+        gsap.to(
+            sphericalCoords, {
+                radius: sc.radius + target.lockedDistance,
+                angle: sc.angle,
+                elevation: sc.elevation,
+                duration: 4,
+                ease: "expo.inOut"
+            }
+        );
+        this.orbit = orbitAround;
+    }
+
+    private measureSCDiff(sc1:ShpericalCoords, sc2:ShpericalCoords):number {
+        let d = Math.abs(sc2.radius-sc1.radius);
+        d += Math.abs(sc1.elevation-sc2.elevation);
+        d += Math.abs(sc1.angle - sc2.angle);
+
+        return d;
     }
 
     private killTweens() {
+        gsap.killTweensOf(sphericalCoords);
         gsap.killTweensOf(this.cam.position);
         gsap.killTweensOf(this.cam.quaternion);
     }
 
     unlock() {
         if(!this.initialized) return console.warn("CameraController not initialized! Please run CameraManager.init() first.");
-        this.mode = CameraMode.DISABLED;
+        this.mode = CameraMode.LOCKED;
         this.killTweens();
         this.orbit = false;
 
-        const pos = TARGET.prevPos;
-        const quat = TARGET.prevRot;
+        this.unlocking = true;
 
-        gsap.to(this.cam.position, {x: pos.x, y: pos.y, z: pos.z, duration: 3, ease: 'power2.inOut', onUpdate:() => {
-            this.cam.lookAt(origin);
-        }, onComplete: () => {
-            this.mode = CameraMode.ORBIT;
-            this.killTweens();
-        }});
-        // gsap.to(this.cam.quaternion, {x: quat.x, y: quat.y, z: quat.z, w: quat.w, duration: 3, ease: 'power2.inOut'});
+        gsap.to(sphericalCoords, {
+            radius: prevSC.radius,
+            angle: prevSC.angle,
+            elevation: prevSC.elevation,
+            duration: 3,
+            ease: 'power2.inOut',
+            onComplete: () => {
+                this.mode = CameraMode.ORBIT;
+                this.killTweens();
+            }
+        });
     }
 
     reset() {
         if(!this.initialized) return console.warn("CameraController not initialized! Please run CameraManager.init() first.");
-        TARGET.prevPos.copy(DEFAULT_CAM_POS);
-        TARGET.prevRot.set(0,0,0,1);
+        const sc = this.getSC(DEFAULT_CAM_POS);
+        this.copySC(sc, prevSC)
         this.unlock();
     }
 
     update() {
         if(!this.initialized) return;
         this.controls.enabled = this.mode === CameraMode.ORBIT;
-        if(this.controls.enabled) this.controls.update();
+        if(this.controls.enabled) {
+            this.controls.update();
+            this.refreshSC();
+        }
         if(this.mode === CameraMode.LOCKED) {
             if(this.orbit) {
                 const t = performance.now() * .001 * .025;
@@ -127,11 +218,18 @@ class CameraController {
                 tmp.z += this.currentTarget.lockedDistance * Math.sin(t);
                 tmp.y += .25 * Math.sin(t * 1.5);
                 this.cam.position.lerp(tmp, CameraSettings.traveling.easing);
-                this.cam.lookAt(this.currentTarget.target.position);
+                // this.cam.lookAt(this.currentTarget.target.position);
             } else {
+                this.updateTarget();
                 this.cam.position.lerp(TARGET.obj.position, CameraSettings.traveling.easing);
-                this.cam.lookAt(origin);
+                if(this.unlocking) {
+                    if(this.measureSCDiff(sphericalCoords, prevSC) < .0001) {
+                        this.killTweens();
+                        this.mode = CameraMode.ORBIT;
+                    }
+                }
             }
+            this.cam.lookAt(origin);
         }
     }
 }
