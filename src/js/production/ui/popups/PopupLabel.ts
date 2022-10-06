6623023832f252d0fcdd3f9c78f4680aac691f67
Edu Prats <eduprats@gmail.com>
2022-10-06 18:22:57 +0200

    Fixed camera lock on planets

--- src/js/production/ui/popups/PopupLabel.ts
+++ src/js/production/ui/popups/PopupLabel.ts
@@ -48,7 +48,7 @@ export class PopupLabel {
 		this.ref.selected = true;
 		this.dom.classList.add('selected');
 		// if(this.ref.closeUp) CameraManager.goToTarget(this.ref);
-		CameraManager.goToTarget(this.ref, false, true);
+		CameraManager.goToTarget(this.ref, false, !this.ref.closeUp);
 	}
 
 	unselect(){
